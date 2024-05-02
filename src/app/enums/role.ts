8faa59eacddc0df116c5049888a7091fab35da42
joelsahli <joel.sahli@infosoft.swiss>
2024-05-02 19:00:32 +0200

    added functionality and enums

---
+++ src/app/enums/role.ts
@@ -0,0 +1,7 @@
+export enum Role {
+    'Top' = 1,
+    'Jungle' = 2,
+    'Mid' = 3,
+    'ADC' = 4,
+    'Support' = 5
+  }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,17 +4,46 @@ import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
   form: FormGroup = this.fb.group({
-    rows: this.fb.array([])
+    rows: this.fb.array([]),
   });
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder) {}
+
+  sortedStack1: any[] = [];
+  sortedStack2: any[] = [];
 
   ngOnInit() {
-    this.addRow();
+    if (typeof localStorage !== 'undefined') {
+      const storedForm = localStorage.getItem('form');
+      if (storedForm) {
+        const parsedForm = JSON.parse(storedForm);
+        this.form = this.fb.group({
+          rows: this.fb.array(
+            parsedForm.rows.map(
+              (row: {
+                name: string;
+                rank: string;
+                role1: string;
+                role2: string;
+              }) => this.fb.group(row)
+            )
+          ),
+        });
+      } else {
+        this.addRow();
+      }
+
+      this.form.valueChanges.subscribe((value) => {
+        localStorage.setItem('form', JSON.stringify(value));
+      });
+    }
+    if (this.rows.length === 0) {
+      this.addRow();
+    }
   }
 
   get rows(): FormArray {
@@ -26,9 +55,33 @@ export class HomeComponent implements OnInit {
       name: '',
       rank: '',
       role1: '',
-      role2: ''
+      role2: '',
     });
 
     this.rows.push(row);
   }
+
+  deleteRow() {
+    if (this.rows.length > 0) {
+      this.rows.removeAt(this.rows.length - 1);
+    }
+  }
+
+  createStack() {
+    let stack = [];
+    let stack2 = [];
+
+      for (let i = 0; i < this.rows.length; i++) {
+        if(i < 5){
+          stack.push(this.rows.at(i).value);
+        }
+        else{
+          stack2.push(this.rows.at(i).value);
+        }
+      }
+      this.sortedStack1 = stack;
+      this.sortedStack2 = stack2;
+    }
+
+  
 }

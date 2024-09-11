303e0785944f5a8afee2715edf57e6967b6a6414
Joel Sahli <joel.sahli@outlook.com>
2024-09-11 22:23:09 +0200

    finalized design for first publish

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { DonateComponent } from './donate/donate.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent }, 
+  { path: 'about', component: AboutComponent }, 
   { path: 'donate', component: DonateComponent }, 
 
 ];
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -28,16 +28,18 @@ export class HomeComponent implements OnInit {
   showTeams = false;
   attemptedSubmit = false;
   displayErrorMessage = false;
+  errorMessage = '';
   ranks: string[] = [
-    'Iron4', 'Iron3', 'Iron2', 'Iron1',
-    'Bronze4', 'Bronze3', 'Bronze2', 'Bronze1',
-    'Silver4', 'Silver3', 'Silver2', 'Silver1',
-    'Gold4', 'Gold3', 'Gold2', 'Gold1',
-    'Platinum4', 'Platinum3', 'Platinum2', 'Platinum1',
-    'Emerald4', 'Emerald3', 'Emerald2', 'Emerald1',
-    'Diamond4', 'Diamond3', 'Diamond2', 'Diamond1',
+    'Iron 4', 'Iron 3', 'Iron 2', 'Iron 1',
+    'Bronze 4', 'Bronze 3', 'Bronze 2', 'Bronze 1',
+    'Silver 4', 'Silver 3', 'Silver 2', 'Silver 1',
+    'Gold 4', 'Gold 3', 'Gold 2', 'Gold 1',
+    'Platinum 4', 'Platinum 3', 'Platinum 2', 'Platinum 1',
+    'Emerald 4', 'Emerald 3', 'Emerald 2', 'Emerald 1',
+    'Diamond 4', 'Diamond 3', 'Diamond 2', 'Diamond 1',
     'Master', 'Grandmaster', 'Challenger'
-  ];
+];
+
   roles: string[] = ['Top', 'Jungle', 'Mid', 'Adc', 'Support'];
 
   constructor(private fb: FormBuilder, private teamService: TeamService) {
@@ -128,11 +130,18 @@ export class HomeComponent implements OnInit {
     this.teamService.createTeams(teamRequest).subscribe(
       (response: any) => {
         console.log('Received response:', response); // Log the response
-        this.teams = response.teams;
-        this.showTeams = true;
+        if (response.error) {
+          this.errorMessage = response.error;
+          this.displayErrorMessage = true;
+        } else {
+          this.teams = response.teams;
+          this.showTeams = true;
+        }
       },
       (error: any) => {
         console.error('Error creating teams:', error);
+        this.errorMessage = 'An error occurred while creating teams. Please try again.';
+        this.displayErrorMessage = true;
       }
     );
   }

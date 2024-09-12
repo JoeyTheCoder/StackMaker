32ee3cd4b9a99fd06eee4060fe48eea2de185941
Joel Sahli <joel.sahli@outlook.com>
2024-09-12 22:56:34 +0200

    https in prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 // src/environments/environment.prod.ts
 export const environment = {
     production: true,
-    apiUrl: 'http://stackmakerapi.ffgang.ch' // Deployed backend URL for production
+    apiUrl: 'https://stackmakerapi.ffgang.ch' // Ensure this is using HTTPS
   };
   
